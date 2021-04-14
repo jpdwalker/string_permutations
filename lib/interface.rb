8ec0442533272189a421d175e83d1911bbfdc05e
Joe Walker <jpdwalker@gmail.com>
2021-04-14 17:24:16 +0100

    first commit, interface and permuation class

---
+++ lib/interface.rb
@@ -0,0 +1,16 @@
+require_relative "string_permutation"
+
+puts "*** PERMUTATE A STRING ***"
+
+running = true
+
+while running
+  puts "Give me a string to permutate...please (press enter to exit)"
+  player_answer = gets.chomp
+  if player_answer.size >= 1
+    puts permutate_string('', player_answer)
+  else
+    puts 'Thanks for playing, see ya!'
+    running = false
+  end
+end
---
+++ lib/string_permutation.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def permutate_string(result, input)
+  result + input[0] if input.length == 1
+
+  if input.length == 2
+    puts result + input[0] + input[1]
+    puts result + input[1] + input[0]
+  end
+
+  (0...input.length).step(1).each do |i|
+    first = result + input[i]
+    second = (i > 0 ? input[0..(i-1)] : '') + (input[(i+1)..-1] || '')
+    permutate_string(first, second)
+  end
+end
